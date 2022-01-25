ba8e89cced916a1f682933ff9e2e9b9409c07a78
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2022-01-25 16:00:23 +0100

    Slash screen

--- App.js
+++ App.js
@@ -1,30 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import jwtDecode from 'jwt-decode';
 
 import AppNavigator from './app/navigation/AppNavigator';
 import AuthContext from './app/auth/context';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import OfflineNotice from './app/components/OfflineNotice';
-import authStorage from './app/auth/storage'
+import authStorage from './app/auth/storage';
 
-import LoginScreen from './app/screens/LoginScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import RegisterScreen from './app/screens/RegisterScreen';
 
 export default function App() {
   const [user, setUser] = useState();
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-    setUser(jwtDecode(token));
+  const getUser = async () => {
+    setUser(await authStorage.getUser());
   };
-  
 
   useEffect(() => {
-    restoreToken();
+    getUser();
   }, []);
 
   return (
